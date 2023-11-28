72e527f9af855f0acab3bc52d6f35ce2d954d87f
lucas <lucas.alves.supus@outlook.com>
2023-11-27 21:16:11 -0300

    fix: vercel

--- dist/src/routes/products.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.productsRouter = void 0;
-const controllers_1 = require("../controllers");
-function productsRouter(router) {
-    router.get("/products", controllers_1.getProductsController);
-}
-exports.productsRouter = productsRouter;
