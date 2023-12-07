2363b4fc9025ed7f4371edd96a44171b71ce66c7
lucas <lucas.alves.supus@outlook.com>
2023-12-06 23:51:37 -0300

    refactor: remove cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import * as restify from "restify";
 import { createConnection } from "typeorm";
-import corsMiddleware from "restify-cors-middleware";
+// import corsMiddleware from "restify-cors-middleware";
 import { typeorm } from "./typeorm";
 import { routes } from "./routes";
 
@@ -9,16 +9,16 @@ export const server = restify.createServer({
   version: "1.0.0",
 });
 
-const cors = corsMiddleware({
-  preflightMaxAge: 5,
-  origins: ["http://localhost:3000", "https://seuoutrodominio.com"],
-  allowHeaders: ["Authorization"],
-  exposeHeaders: ["Authorization"],
-  credentials: true,
-});
+// const cors = corsMiddleware({
+//   preflightMaxAge: 5,
+//   origins: ["http://localhost:3000", "https://seuoutrodominio.com"],
+//   allowHeaders: ["Authorization"],
+//   exposeHeaders: ["Authorization"],
+//   credentials: true,
+// });
 
-server.pre(cors.preflight);
-server.use(cors.actual);
+// server.pre(cors.preflight);
+// server.use(cors.actual);
 
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
